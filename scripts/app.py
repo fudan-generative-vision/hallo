06cbc240ef062d79bf68c83a03989afbaa38b62f
cocktailpeanut <cocktailpeanuts@proton.me>
2024-06-16 10:01:37 -0400

    update

--- scripts/app.py
+++ scripts/app.py
@@ -13,14 +13,18 @@ def dict_to_namespace(d):
   return namespace
 
 
-def predict(image, audio, size, steps, pose_weight, face_weight, lip_weight, face_expand_ratio):
+def predict(image, audio, size, steps, fps, cfg, pose_weight, face_weight, lip_weight, face_expand_ratio):
   dict = {
     'data': {
       'source_image': {
         'width': size,
         'height': size
+      },
+      'export_video': {
+        'fps': fps
       }
     },
+    'cfg_scale': cfg,
     'source_image': image,
     'driving_audio': audio,
     'pose_weight': pose_weight,
@@ -58,7 +62,9 @@ app = gr.Interface(
       gr.Image(type="filepath"),
       gr.Audio(type="filepath"),
       gr.Number(label="size", value=256, minimum=256, maximum=512, step=64, precision=0),
-      gr.Number(label="steps", value=40, minimum=1, step=1, precision=0),
+      gr.Number(label="steps", value=20, minimum=1, step=1, precision=0),
+      gr.Number(label="fps", value=25, minimum=1, step=1, precision=0),
+      gr.Slider(label="CFG Scale", value=3.5, minimum=0, maximum=10, step=0.01),
       gr.Number(label="pose weight", value=1.0),
       gr.Number(label="face weight", value=1.0),
       gr.Number(label="lip weight", value=1.0),
