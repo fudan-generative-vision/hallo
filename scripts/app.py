01c894136c6e2204ee80fe5477ddcdb025c16fd5
cocktailpeanut <cocktailpeanuts@proton.me>
2024-06-16 10:15:50 -0400

    update

--- scripts/app.py
+++ scripts/app.py
@@ -61,8 +61,8 @@ app = gr.Interface(
     inputs=[
       gr.Image(type="filepath"),
       gr.Audio(type="filepath"),
-      gr.Number(label="size", value=256, minimum=256, maximum=512, step=64, precision=0),
-      gr.Number(label="steps", value=20, minimum=1, step=1, precision=0),
+      gr.Number(label="size", value=512, minimum=256, maximum=512, step=64, precision=0),
+      gr.Number(label="steps", value=40, minimum=1, step=1, precision=0),
       gr.Number(label="fps", value=25, minimum=1, step=1, precision=0),
       gr.Slider(label="CFG Scale", value=3.5, minimum=0, maximum=10, step=0.01),
       gr.Number(label="pose weight", value=1.0),
