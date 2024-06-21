d5fc700f8a549641eff4381b62e0dfdcb01f26e0
leeway.zlw <leeway.zlw@outlook.com>
2024-06-21 11:31:56 +0800

    fix: optimize gradio demo. remove unnecessary options

--- scripts/app.py
+++ scripts/app.py
@@ -15,21 +15,12 @@ import gradio as gr
 from inference import inference_process
 
 
-def predict(image, audio, size, steps, fps, cfg, pose_weight, face_weight, lip_weight, face_expand_ratio):
+def predict(image, audio, pose_weight, face_weight, lip_weight, face_expand_ratio, progress=gr.Progress(track_tqdm=True)):
     """
     Create a gradio interface with the configs.
     """
+    _ = progress
     config = {
-        'data': {
-            'source_image': {
-                'width': size,
-                'height': size
-            },
-            'export_video': {
-                'fps': fps
-            }
-        },
-        'cfg_scale': cfg,
         'source_image': image,
         'driving_audio': audio,
         'pose_weight': pose_weight,
@@ -38,8 +29,7 @@ def predict(image, audio, size, steps, fps, cfg, pose_weight, face_weight, lip_w
         'face_expand_ratio': face_expand_ratio,
         'config': 'configs/inference/default.yaml',
         'checkpoint': None,
-        'output': ".cache/output.mp4",
-        'inference_steps': steps
+        'output': ".cache/output.mp4"
     }
     args = argparse.Namespace()
     for key, value in config.items():
@@ -51,10 +41,6 @@ app = gr.Interface(
     inputs=[
       gr.Image(label="source image (no webp)", type="filepath", format="jpeg"),
       gr.Audio(label="source audio", type="filepath"),
-      gr.Number(label="size", value=512, minimum=256, maximum=512, step=64, precision=0),
-      gr.Number(label="steps", value=40, minimum=1, step=1, precision=0),
-      gr.Number(label="fps", value=25, minimum=1, step=1, precision=0),
-      gr.Slider(label="CFG Scale", value=3.5, minimum=0, maximum=10, step=0.01),
       gr.Number(label="pose weight", value=1.0),
       gr.Number(label="face weight", value=1.0),
       gr.Number(label="lip weight", value=1.0),
